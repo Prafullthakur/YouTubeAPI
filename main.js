5bfaa2510afef6fa462c03d7b815764c7c63fd36
prafull <prafullchuahn45@gmail.com>
2020-04-07 18:21:33 +0530

    add

--- main.js
+++ main.js
@@ -31,16 +31,16 @@ function updateSigninStatus(isSignedIn)
 {
 	if(isSignedIn)
 	{
-		authorizeButton.style.display='block';
-		signoutButton.style.display='none';
+		authorizeButton.style.display='none';
+		signoutButton.style.display='block';
 		content.style.display='block';
 		videoContainer.style.display='block';
 		getChannel(defaultChannel);
 	}
 	else
 	{
-		authorizeButton.style.display='none';
-		signoutButton.style.display='block';
+		authorizeButton.style.display='block';
+		signoutButton.style.display='none';
 		videoContainer.style.display='none';
 		content.style.display='none';
 	}	
