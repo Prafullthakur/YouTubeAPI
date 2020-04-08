1f24ff12f052cff806e8fc9c57296c82b0a1e6fa
prafull <prafullchuahn45@gmail.com>
2020-04-08 15:49:09 +0530

    add

--- main.js
+++ main.js
@@ -108,22 +108,16 @@ function requestVideoPlaylist(ID)
    	  if(playlistItems)
    	  {
            let output = '<h4 class="center-align">Latest Videos</h4>';
-           const videoId = playlistItems.snippet.resourceId.videoId;
-           output += `
-             <div class="col s3">
-             <iframe width="100%" height="auto" src="https:/www.youtube.com/embed/${"oytFceSq9Rs"}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-             </div>
-             `;
-           /*playlistItems.forEach(item =>{
+           playlistItems.forEach(item =>{
            
             const videoId = item.snippet.resourceId.videoId;
               console.log(videoId);
-             output += `
+             /*output += `
              <div class="col s3">
              <iframe width="100%" height="auto" src="https:/www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
              </div>
-           `
-       });*/
+           `*/
+       });
            videoContainer.innerHTML = output;
    	  }
    	  else{
